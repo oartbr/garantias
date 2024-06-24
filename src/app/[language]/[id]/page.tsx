6f68ce80ac89b503ab398adf2f0d7478d131afcf
Alex Sevilla <alex.sevilla@gmail.com>
2024-06-24 13:38:52 -0300

    refactor: remove unused check code function in page.tsx

--- src/app/[language]/[id]/page.tsx
+++ src/app/[language]/[id]/page.tsx
@@ -16,14 +16,6 @@ type Props = {
   params: { language: string; id: string };
 };
 
-export async function checkCode({ params }: Props): Promise<Metadata> {
-  const { t } = await getServerTranslation(params.language, "home");
-
-  return {
-    title: t("title"),
-  };
-}
-
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { t } = await getServerTranslation(params.language, "home");
 

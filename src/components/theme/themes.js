f8ab7828a976f3e837194df0df922aa99284a5cb
Alex Sevilla <alex.sevilla@gmail.com>
2025-04-01 11:43:24 -0300

    feat: update default logo URL for improved branding

--- src/components/theme/themes.js
+++ src/components/theme/themes.js
@@ -11,7 +11,7 @@ ClientThemes.default = ClientThemes[process.env.CLIENT_THEME];
 
 export const LOGO =
   process.env.CLIENT_LOGO ||
-  "https://xvzq0akbnljx2cl9.public.blob.vercel-storage.com/themes/wse/logo.WSE.short-4f040biXYVrLi7cp1IfJzGMyLFdGgj.svg";
+  "https://xvzq0akbnljx2cl9.public.blob.vercel-storage.com/themes/wse/logo.wse.short.white-pn6QrG6ag6In2ivoueODvCs3mQ85Hm.svg";
 
 const GetTheme = () => {
   return ClientThemes.default;
